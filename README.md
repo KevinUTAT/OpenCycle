afab01e4d30b31c9ce5919de7a3d72c008442148
KevinUTAT <wenkai.xu@mail.utoronto.ca>
2021-10-30 16:48:54 -0400

    Add speed and distance

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # What is this?
 This project is to create a fully open sourced cycling computer based on micro-controller hardwares such as ESP32 and Pi Pico. \
-Similar project exist on ARM-Linux platforms but I want something that can be run on very miniature hardware while still maintain good battery life. \
 This project is still a working progress and are at a very early stage so please forgive me for the current state.\
-![](resource/opencyclev001.png)
+![](resource/opencyclev002.png)
+# Why Open Cycle?
+Similar project exist on ARM-Linux platforms but I want something that can be run on very miniature hardware while still maintain good battery life. \
+One of the goal for this project is to create a software platform to make it easier to make your own cycling computer with your choices of hardware and interface.\
+If you have a project with specific hardware and sensor in mind, feel free to try this out or open an issue so I can help as well. 
 # Hardware compatibility
 You will need a MicroPython compatible micro-controller such as one based on the ESP-32 module or the Pi-Pico module.\
 Currently, I have only tested it on the [*DIYMore OLED ESP32 Board*](https://www.diymore.cc/products/diymore-esp32-0-96-inch-oled-display-wifi-bluetooth-18650-battery-shield-development-board-cp2102-module-for-arduino) I don't actually recommend this board but its what I have on hands right now and more tested board are coming.\
@@ -18,5 +21,7 @@ If you have a SSD1306 OLED display on I2C, run *open_cycle.run_openCycle*.\
 Or run *open_cycle.run_openCycle(on_display=False)* if you just want data to be printed.\
 You might need to run it more then once to have the bluetooth connection to be connected :(
 # Change log:
+### 2021-10-30:
+Add speed and distance calculation
 ### 2021-10-24:
 Finally getting data from the speed/Cadence sensor
--- open_cycle.py
+++ open_cycle.py
@@ -4,6 +4,8 @@ import ssd1306
 
 MS_IN_MIN = 60000
 SHORT_SIZE = 65536
+WHEEL_CIRC = 2155 # mm, for 32-622 (700x32C)
+KPH2MMPM = 16667 # km/h to mm/min
 
 # using default address 0x3C
 i2c = I2C(sda=Pin(5), scl=Pin(4))
@@ -11,6 +13,8 @@ display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 wheelRev = 0
 lastUpdate = 0
+
+rev_count = 0
 
 def display_msg(title, msg0="", msg1="", msg2=""):
     display.fill(0)  
@@ -21,15 +25,19 @@ def display_msg(title, msg0="", msg1="", msg2=""):
     display.text(msg2, 0, 50, 1)
     display.show()
 
-def run_display(raw_data, rpm):
+def run_debug_display(raw_data, rpm, speed, distance):
     display.fill(0)  
     display.text('OpenCycle', 0, 0, 1)
-    display.text('Wheel Rev: ', 0, 20, 1)
-    display.text(str(raw_data[1]), 80, 20, 1)
-    display.text('Time: ', 0, 30, 1)
-    display.text(str(raw_data[2]), 50, 30, 1)
-    display.text('RPM: ', 0, 40, 1)
-    display.text(str(rpm), 50, 40, 1)
+    display.text('Wheel Rev: ', 0, 10, 1)
+    display.text(str(raw_data[1]), 80, 10, 1)
+    display.text('Time: ', 0, 20, 1)
+    display.text(str(raw_data[2]), 50, 20, 1)
+    display.text('RPM: ', 0, 30, 1)
+    display.text(str(rpm), 50, 30, 1)
+    display.text('Speed: ', 0, 40, 1)
+    display.text(str(speed), 50, 40, 1)
+    display.text('Distance: ', 0, 50, 1)
+    display.text(str(distance), 70, 50, 1)
     display.show()
 
 def print_data(data):
@@ -39,12 +47,14 @@ def print_data(data):
 def on_receive_CSC(csc_data):
     global wheelRev
     global lastUpdate
+    global rev_count
     # if its the first notification on power up
     if (wheelRev == 0) and (lastUpdate == 0):
         wheelRev = csc_data[1]
         lastUpdate = csc_data[2]
         return
     rev = csc_data[1] - wheelRev
+    rev_count += rev
     # handles wraparound
     if lastUpdate > csc_data[2]:
         dt = csc_data[2] + SHORT_SIZE - lastUpdate
@@ -54,12 +64,16 @@ def on_receive_CSC(csc_data):
         rpm = 0
     else:
         rpm = (rev / dt) * MS_IN_MIN
+        
+    speed = (rpm * WHEEL_CIRC) / KPH2MMPM
+    dist = (rev_count * WHEEL_CIRC) / 1000000
     
     # update global vars
     wheelRev = csc_data[1]
     lastUpdate = csc_data[2]
 
-    run_display(csc_data, rpm)
+    run_debug_display(csc_data, rpm, speed, dist)
+    
     
 def run_openCycle(on_display=True):
     if on_display:
